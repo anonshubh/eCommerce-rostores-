aedbf3499f686cd835ccdc9b52661ccd8a71d6a9
anonshubh <shubhpathak07@gmail.com>
2020-04-26 21:50:57 +0530

    static files error(2)

--- ecommerce_src/settings/production.py
+++ ecommerce_src/settings/production.py
@@ -171,3 +171,6 @@ CSRF_COOKIE_SECURE              = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
+
+import django_heroku
+django_heroku.settings(locals())
